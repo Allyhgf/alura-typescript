5ba82afade405f78497c3e75ffde93b15137b985
Allyson Hansel <allysongithub21@gmail.com>
2022-10-01 17:56:39 -0300

    Mais tipagem e organização de código

--- app/controllers/negociacao-contoller.ts
+++ app/controllers/negociacao-contoller.ts
@@ -11,15 +11,18 @@ export class NegociacaoController {
         this.inputValor = document.querySelector("#valor");
     }
 
-    adicionar(){
+    adicionar(): void{
+        const negociacao = this.criaNegociaciao();
+        console.log(negociacao);
+    }
+
+    criaNegociaciao(): Negociacao{
         const exp = /-/g;
 
         const date = new Date(this.inputData.value.replace(exp, ","));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
 
-        const negociacao = new Negociacao(date, quantidade, valor);
-
-        console.log(negociacao);
+        return new Negociacao(date, quantidade, valor);
     }
 }
